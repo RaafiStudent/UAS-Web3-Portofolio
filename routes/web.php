fd436a022743d1a547670b5be8530f66d4426de4
raafi <raafistudent@gmail.com>
2026-01-11 17:31:26 +0700

    Membuat Logic ProjectController dan Route Resource

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,30 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\AdminController; // <-- Panggil Controller Admin
+use App\Http\Controllers\ProjectController; // <--- JANGAN LUPA INI DITAMBAHKAN
 use App\Models\Project;
 use App\Models\Skill;
 use Illuminate\Support\Facades\Route;
 
-// HALAMAN DEPAN (PUBLIC)
 Route::get('/', function () {
     return view('welcome', [
-        'projects' => Project::all(), // Kirim data project ke depan
-        'skills' => Skill::all()      // Kirim data skill ke depan
+        'projects' => Project::all(),
+        'skills' => Skill::all()
     ]);
 });
 
-// HALAMAN ADMIN (DASHBOARD) - HANYA BISA DIAKSES KALAU LOGIN
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// GROUPING ROUTE ADMIN (Biar rapi)
+// GROUP ADMIN
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    // Nanti kita tambah route CRUD Project di sini
+
+    // CRUD PROJECTS (Baris Sakti ini Boss!)
+    Route::resource('projects', ProjectController::class);
 });
 
 require __DIR__.'/auth.php';
