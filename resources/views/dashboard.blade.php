3d3ac8bde8bbfc1eb6cb8275a1e62ab3dfd8c4d6
raafi <raafistudent@gmail.com>
2026-01-11 18:51:59 +0700

    Perbaiki Dashboard Admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-cyan-400 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+            {{-- KOTAK DASHBOARD --}}
+            <div class="bg-gray-800 border border-gray-700 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-100">
+                    {{ __("Selamat Datang, Boss! Anda sudah login.") }}
                 </div>
             </div>
         </div>
