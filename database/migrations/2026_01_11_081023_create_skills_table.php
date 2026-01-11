fa71456b69a6881b5726e3c04ad97299b986e3d3
raafi <raafistudent@gmail.com>
2026-01-11 15:33:23 +0700

    Membuat Schema Database dan Model untuk Skill

--- database/migrations/2026_01_11_081023_create_skills_table.php
+++ database/migrations/2026_01_11_081023_create_skills_table.php
@@ -6,20 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('skills', function (Blueprint $table) {
             $table->id();
+            $table->string('name');             // Nama Skill (Laravel, CSS)
+            $table->string('color')->default('blue'); // Warna badge (opsional)
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('skills');
