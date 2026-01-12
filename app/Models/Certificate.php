2462c10c607cc65ac79a7c512ccd71d6040933fa
raafi <raafistudent@gmail.com>
2026-01-12 09:54:58 +0700

    model sertifikat

--- app/Models/Certificate.php
+++ app/Models/Certificate.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Certificate extends Model
 {
-    //
+    use HasFactory;
+
+    // WAJIB DITAMBAHKAN BIAR BISA SIMPAN DATA
+    protected $fillable = [
+        'title', 
+        'issuer', 
+        'issued_at', 
+        'description', 
+        'image'
+    ];
 }
